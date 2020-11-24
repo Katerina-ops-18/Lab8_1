405ac5c2d5dd82278cf527a1a162d688d5a987dc
Katerina-ops-18 <61978136+Katerina-ops-18@users.noreply.github.com>
2020-11-24 14:43:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab8_1
+{
+    class Apple
+    {
+        public string Name { get; set; }
+    }
+    class Fruits
+    {
+        Apple[] data;
+
+        public Fruits()
+        {
+            data = new Apple[6];
+        }
+        public Apple this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruits fruits = new Fruits();
+            fruits[0] = new Apple() { Name = "x" };
+            fruits[1] = new Apple() { Name = "y" };
+            Apple y = fruits[1];
+            Apple x = fruits[0];
+            Console.WriteLine(y?.Name);
+            Console.WriteLine(x?.Name);
+            Console.ReadKey();
+        }
+
+    }
+}
